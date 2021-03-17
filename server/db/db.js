cd55f9e02f220bdd1c708bb86a45f5e948cab6fe
John Vitales <JohnieeJuiceboxx@gmail.com>
2021-03-16 21:19:09 -0400

    updated sequelize protocol

--- server/db/db.js
+++ server/db/db.js
@@ -6,7 +6,12 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres'
+    // dialectOptions: {
+    //   ssl: true
+    // }
   }
 )
 module.exports = db
