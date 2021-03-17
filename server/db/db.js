4a5134f2dfe63b3d7c0df679f406c050232a3df2
John Vitales <JohnieeJuiceboxx@gmail.com>
2021-03-16 21:33:21 -0400

    updating sequelize ssl mode

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,8 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    `postgres://localhost:5432/${databaseName}?sslmode=require`,
   {
     logging: false,
     dialect: 'postgres',
